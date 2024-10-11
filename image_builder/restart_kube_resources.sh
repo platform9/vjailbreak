7487c4d5791f4f96a6b957eba9a928351e7f9237
tanaypf9 <tanay@platform9.com>
2024-10-11 11:24:29 -0700

    Update startup script

--- image_builder/restart_kube_resources.sh
+++ image_builder/restart_kube_resources.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-k3s token rotate
+kubectl delete node v2v-helper
 kubectl get deployments --all-namespaces | tail +2 | awk '{ cmd=sprintf("kubectl rollout restart deployment -n %s %s", $1, $2) ; system(cmd) }'
 kubectl get daemonsets --all-namespaces | tail +2 | awk '{ cmd=sprintf("kubectl rollout restart daemonsets -n %s %s", $1, $2) ; system(cmd) }'
 sleep 20
 kubectl get pods --all-namespaces | grep Terminating | awk '{print $1" "$2}' | xargs -n 2 kubectl delete pod --grace-period=0 --force --namespace
-
