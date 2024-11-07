7baf920a9a639de2d30155218497b98629e52bd4
Kimberly Nguy <kimnguyy@gmail.com>
2024-11-07 15:38:54 -0800

    Add grafana button to appbar and remove package.lock file

--- ui/src/components/AppBar.tsx
+++ ui/src/components/AppBar.tsx
@@ -5,22 +5,37 @@ import Button from "@mui/material/Button"
 import Toolbar from "@mui/material/Toolbar"
 
 export default function ButtonAppBar({ setOpenMigrationForm, hide = false }) {
+  const openGrafanaDashboard = () => {
+    window.open(`https://${window.location.host}/grafana`, "_blank")
+  }
+
+  const openMigrationForm = () => {
+    setOpenMigrationForm(true)
+  }
+
   return (
     <Box sx={{ visibility: hide ? "hidden" : "visible" }}>
       <AppBar position="static" color="transparent">
         <Toolbar>
           <Typography variant="h3">vJailbreak</Typography>
-          <Button
-            size="large"
-            onClick={() => setOpenMigrationForm(true)}
-            color="primary"
-            variant="contained"
-            sx={{
-              marginLeft: "auto",
-            }}
-          >
-            Start Migration
-          </Button>
+          <Box sx={{ display: "flex", gap: 2, marginLeft: "auto" }}>
+            <Button
+              size="large"
+              onClick={openGrafanaDashboard}
+              color="primary"
+              variant="outlined"
+            >
+              Open Grafana
+            </Button>
+            <Button
+              size="large"
+              onClick={openMigrationForm}
+              color="primary"
+              variant="contained"
+            >
+              Start Migration
+            </Button>
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
