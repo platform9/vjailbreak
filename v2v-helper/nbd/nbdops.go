bfef772a1744a9c825316096eb80bb977f473e51
tanaypf9 <tanay@platform9.com>
2024-12-18 07:14:48 -0800

    Initial KVM acceleration changes

--- v2v-helper/nbd/nbdops.go
+++ v2v-helper/nbd/nbdops.go
@@ -118,7 +118,6 @@ vixDiskLib.nfcAio.Session.BufCount=4`
 	// Log the command
 	cmdstring := ""
 	for _, arg := range cmd.Args {
-
 		if strings.Contains(arg, password) {
 			cmdstring += "password=[REDACTED] "
 		} else {
