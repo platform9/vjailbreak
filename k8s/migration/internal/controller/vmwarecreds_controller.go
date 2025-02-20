0be3e7bab451f4fa4630493e80a34b798788128e
Omkar Deshpande <47773862+OmkarDeshpande7@users.noreply.github.com>
2025-02-20 17:23:39 +0530

    fix vmwarecreds npe (#194)

--- k8s/migration/internal/controller/vmwarecreds_controller.go
+++ k8s/migration/internal/controller/vmwarecreds_controller.go
@@ -244,6 +244,10 @@ func GetAllVMs(ctx context.Context, vmwcreds *vjailbreakv1alpha1.VMwareCreds, da
 		var disks []string
 		var ds mo.Datastore
 		var dsref types.ManagedObjectReference
+		if vmProps.Config == nil {
+			// VM is not powered on or is in creating state
+			continue
+		}
 		for _, device := range vmProps.Config.Hardware.Device {
 			switch dev := device.(type) {
 			case *types.VirtualE1000e:
