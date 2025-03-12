8cf35728de33b5a731af6c60aee2bd7fdc3ede2e
Abhijeet Thakur <abhijeet.thakur@indexnine.com>
2025-03-12 21:47:22 +0530

    Bug :: #269 :: Unable to use uppercase letters in a credential's name (#273)

--- ui/src/utils.ts
+++ ui/src/utils.ts
@@ -1,8 +1,8 @@
-import { either, isEmpty, isNil } from "ramda"
+import { either, isEmpty, isNil } from "ramda";
 
-export const isNilOrEmpty = either(isNil, isEmpty)
+export const isNilOrEmpty = either(isNil, isEmpty);
 
-const duplicatedSlashesRegexp = new RegExp("(^\\/|[^:\\/]+\\/)\\/+", "g")
+const duplicatedSlashesRegexp = new RegExp("(^\\/|[^:\\/]+\\/)\\/+", "g");
 
 // Given some path segments returns a properly formatted path similarly to Nodejs path.join()
 // Remove duplicated slashes
@@ -12,20 +12,39 @@ export const pathJoin = (...pathParts) =>
     .concat(...pathParts) // Flatten
     .filter((segment) => !!segment) // Remove empty parts
     .join("/")
-    .replace(duplicatedSlashesRegexp, "$1")
+    .replace(duplicatedSlashesRegexp, "$1");
+
+/**
+ * Checks if the given string is a valid Resource credential name.
+ *
+ * A valid Resource credential name is a string of up to 253 characters which
+ * conforms to the following rules:
+ *
+ * 1. The first character must be a letter (a-z) or a number (0-9).
+ * 2. The remaining characters must be letters, numbers, or hyphens.
+ * 3. The string must end with a letter or number.
+ *
+ * @param {string} name The string to check.
+ * @returns {boolean} Whether the string is a valid Resource credential name.
+ */
+export const isValidName = (name: string) =>
+  (/^[a-z0-9]([-a-z0-9]*[a-z0-9])?$/.test(name) &&
+    name.length <= 253 &&
+    name.length > 0) ||
+  !name;
 
 export const debounce = (func, delay) => {
-  let timeout
+  let timeout;
 
   const debouncedFunction = (...args) => {
-    clearTimeout(timeout)
-    timeout = setTimeout(() => func(...args), delay)
-  }
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), delay);
+  };
 
   // Add a cancel method to clear the timeout
   debouncedFunction.cancel = () => {
-    clearTimeout(timeout)
-  }
+    clearTimeout(timeout);
+  };
 
-  return debouncedFunction
-}
+  return debouncedFunction;
+};
