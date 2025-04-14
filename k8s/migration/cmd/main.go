a88374ecee773a7dda8a0802a09ab6bae20d7037
Omkar Deshpande <omideshpande77@gmail.com>
2025-04-14 06:39:13 +0000

    basic bm-config

--- k8s/migration/cmd/main.go
+++ k8s/migration/cmd/main.go
@@ -126,6 +126,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "ClusterMigration")
 		os.Exit(1)
 	}
+	if err = (&controller.BMConfigReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "BMConfig")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err = mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
