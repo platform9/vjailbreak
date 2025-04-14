f4ee7bafd621bc43a0c7bed3a00a104b89ad929f
Omkar Deshpande <47773862+OmkarDeshpande7@users.noreply.github.com>
2025-04-14 14:57:16 +0530

    Cordon esxi (#365)

--- k8s/migration/internal/controller/openstackcreds_controller.go
+++ k8s/migration/internal/controller/openstackcreds_controller.go
@@ -126,7 +126,7 @@ func (r *OpenstackCredsReconciler) reconcileNormal(ctx context.Context,
 		} else {
 			ctxlog.Info("Successfully updated master node image ID")
 		}
-		openstackCredential, err := utils.GetOpenstackCredentials(ctx, r.Client, scope.OpenstackCreds.Spec.SecretRef.Name)
+		openstackCredential, err := utils.GetOpenstackCredentialsFromSecret(ctx, r.Client, scope.OpenstackCreds.Spec.SecretRef.Name)
 		if err != nil {
 			ctxlog.Error(err, "Failed to get OpenStack credentials from secret", "secretName", scope.OpenstackCreds.Spec.SecretRef.Name)
 			return ctrl.Result{}, errors.Wrap(err, "failed to get Openstack credentials from secret")
