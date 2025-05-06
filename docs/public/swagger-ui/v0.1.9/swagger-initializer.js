de09911cdab5503d5cc538ef86178bd14e76dac8
sarikap9 <sarika@platform9.com>
2025-05-06 16:41:20 +0530

    Added Swagger UI to github pages for v0.1.9

--- docs/public/swagger-ui/v0.1.9/swagger-initializer.js
+++ docs/public/swagger-ui/v0.1.9/swagger-initializer.js
@@ -1,6 +1,6 @@
 window.onload = function() {
   window.ui = SwaggerUIBundle({
-    url: "./openapi.yaml",
+    url: "/openapi.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [ SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset ],
