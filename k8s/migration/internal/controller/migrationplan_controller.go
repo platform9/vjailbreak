4b3846556a38faf6ab14210828714fc8aec38ce9
Suhas <suhas@platform9.com>
2025-05-06 20:16:45 +0530

    remove log

--- k8s/migration/internal/controller/migrationplan_controller.go
+++ k8s/migration/internal/controller/migrationplan_controller.go
@@ -653,8 +653,7 @@ func (r *MigrationPlanReconciler) reconcileNetwork(ctx context.Context,
 			}
 		}
 	}
-	fmt.Println("openstacknws", openstacknws)
-	fmt.Println("vmnws", vmnws)
+
 	if len(openstacknws) != len(vmnws) {
 		return nil, fmt.Errorf("VMware Network(s) not found in NetworkMapping")
 	}
