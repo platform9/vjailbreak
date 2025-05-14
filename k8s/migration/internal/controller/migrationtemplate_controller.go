6c4bf6e81e8d08439d063683fa83cd9d2d70a33a
Omkar Deshpande <47773862+OmkarDeshpande7@users.noreply.github.com>
2025-05-14 14:32:22 +0530

    remove stale esxi entry (#470)

--- k8s/migration/internal/controller/migrationtemplate_controller.go
+++ k8s/migration/internal/controller/migrationtemplate_controller.go
@@ -45,13 +45,6 @@ type MigrationTemplateReconciler struct {
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the MigrationTemplate object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.18.4/pkg/reconcile
 func (r *MigrationTemplateReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	r.ctxlog = log.FromContext(ctx)
 	r.ctxlog.Info("Reconciling MigrationTemplate")
