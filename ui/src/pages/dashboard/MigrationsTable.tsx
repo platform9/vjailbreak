e10200a20127dcc1332278224bfa0de45a804358
K Suhas Pai <suhas@platform9.com>
2025-05-23 16:21:53 +0530

    User should be able to delete migration when in pending or any stage (#511)

--- ui/src/pages/dashboard/MigrationsTable.tsx
+++ ui/src/pages/dashboard/MigrationsTable.tsx
@@ -89,20 +89,17 @@ const columns: GridColDef[] = [
         headerName: "Actions",
         flex: 1,
         renderCell: (params) => {
-            const phase = params.row?.status?.phase;
-            const isDisabled = !phase || phase === "Running" || phase === "Pending";
 
             return (
-                <Tooltip title={isDisabled ? "Cannot delete while migration is in progress" : "Delete migration"} >
+                <Tooltip title={"Delete migration"} >
                     <IconButton
                         onClick={(e) => {
                             e.stopPropagation();
                             params.row.onDelete(params.row.metadata?.name);
                         }}
-                        disabled={isDisabled}
                         size="small"
                         sx={{
-                            cursor: isDisabled ? 'not-allowed' : 'pointer',
+                            cursor: 'pointer',
                             position: 'relative'
                         }}
                     >
@@ -181,11 +178,6 @@ export default function MigrationsTable({
         onDelete: onDeleteMigration
     })) || [];
 
-    const isRowSelectable = (params) => {
-        const phase = params.row?.status?.phase;
-        return !(!phase || phase === "Running" || phase === "Pending");
-    };
-
     return (
         <DataGrid
             rows={migrationsWithActions}
@@ -200,7 +192,6 @@ export default function MigrationsTable({
             localeText={{ noRowsLabel: "No Migrations Available" }}
             getRowId={(row) => row.metadata?.name}
             checkboxSelection
-            isRowSelectable={isRowSelectable}
             onRowSelectionModelChange={handleSelectionChange}
             rowSelectionModel={selectedRows}
             slots={{
