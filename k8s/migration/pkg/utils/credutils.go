8db24e3b1c1db821ca5b0946161eaf22f31cfda7
Suhas <suhas@platform9.com>
2025-05-29 17:24:19 +0530

    lint fix

--- k8s/migration/pkg/utils/credutils.go
+++ k8s/migration/pkg/utils/credutils.go
@@ -761,7 +761,7 @@ func CreateOrUpdateVMwareMachine(ctx context.Context, client client.Client,
 	return nil
 }
 
-func GetClosestFlavour(ctx context.Context, cpu, memory int, computeClient *gophercloud.ServiceClient) (*flavors.Flavor, error) {
+func GetClosestFlavour(cpu, memory int, computeClient *gophercloud.ServiceClient) (*flavors.Flavor, error) {
 	allPages, err := flavors.ListDetail(computeClient, nil).AllPages()
 	if err != nil {
 		return nil, fmt.Errorf("failed to list flavors: %w", err)
