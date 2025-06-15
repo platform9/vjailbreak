6af439b384a60d318bf411d32d698e214c77afcf
K Suhas Pai <suhas@platform9.com>
2025-06-15 15:10:45 +0530

    Changed block don't fail immediately, try again.  (#577)

--- v2v-helper/vm/vmops.go
+++ v2v-helper/vm/vmops.go
@@ -5,6 +5,7 @@ package vm
 import (
 	"context"
 	"fmt"
+	"log"
 	"strings"
 	"time"
 
@@ -23,7 +24,8 @@ import (
 type VMOperations interface {
 	GetVMInfo(ostype string) (VMInfo, error)
 	GetVMObj() *object.VirtualMachine
-	UpdateDiskInfo(vminfo VMInfo) (VMInfo, error)
+	UpdateDiskInfo(*VMInfo, VMDisk, bool) error
+	UpdateDisksInfo(*VMInfo) error
 	IsCBTEnabled() (bool, error)
 	EnableCBT() error
 	TakeSnapshot(name string) error
@@ -186,7 +188,7 @@ func getChangeID(disk *types.VirtualDisk) (*ChangeID, error) {
 	return parseChangeID(changeId)
 }
 
-func (vmops *VMOps) UpdateDiskInfo(vminfo VMInfo) (VMInfo, error) {
+func (vmops *VMOps) UpdateDisksInfo(vminfo *VMInfo) error {
 	pc := vmops.vcclient.VCPropertyCollector
 	vm := vmops.VMObj
 	var snapbackingdisk []string
@@ -196,7 +198,7 @@ func (vmops *VMOps) UpdateDiskInfo(vminfo VMInfo) (VMInfo, error) {
 	var o mo.VirtualMachine
 	err := vm.Properties(vmops.ctx, vm.Reference(), []string{}, &o)
 	if err != nil {
-		return vminfo, fmt.Errorf("failed to get VM properties: %s", err)
+		return fmt.Errorf("failed to get VM properties: %s", err)
 	}
 
 	if o.Snapshot != nil {
@@ -204,7 +206,7 @@ func (vmops *VMOps) UpdateDiskInfo(vminfo VMInfo) (VMInfo, error) {
 		var s mo.VirtualMachineSnapshot
 		err := pc.RetrieveOne(vmops.ctx, o.Snapshot.CurrentSnapshot.Reference(), []string{}, &s)
 		if err != nil {
-			return vminfo, fmt.Errorf("failed to get snapshot properties: %s", err)
+			return fmt.Errorf("failed to get snapshot properties: %s", err)
 		}
 
 		for _, device := range s.Config.Hardware.Device {
@@ -216,7 +218,7 @@ func (vmops *VMOps) UpdateDiskInfo(vminfo VMInfo) (VMInfo, error) {
 				snapname = append(snapname, o.Snapshot.CurrentSnapshot.Value)
 				changeid, err := getChangeID(disk)
 				if err != nil {
-					return vminfo, fmt.Errorf("failed to get change ID: %s", err)
+					return fmt.Errorf("failed to get change ID: %s", err)
 				}
 				snapid = append(snapid, changeid.Value)
 			}
@@ -228,7 +230,61 @@ func (vmops *VMOps) UpdateDiskInfo(vminfo VMInfo) (VMInfo, error) {
 		}
 	}
 
-	return vminfo, nil
+	return nil
+}
+
+func (vmops *VMOps) UpdateDiskInfo(vminfo *VMInfo, disk VMDisk, blockCopySuccess bool) error {
+	pc := vmops.vcclient.VCPropertyCollector
+	vm := vmops.VMObj
+	var snapbackingdisk []string
+	var snapname []string
+	var snapid []string
+
+	var o mo.VirtualMachine
+	err := vm.Properties(vmops.ctx, vm.Reference(), []string{}, &o)
+	if err != nil {
+		return fmt.Errorf("failed to get VM properties: %s", err)
+	}
+
+	if o.Snapshot != nil {
+		// get backing disk of snapshot
+		var s mo.VirtualMachineSnapshot
+		err := pc.RetrieveOne(vmops.ctx, o.Snapshot.CurrentSnapshot.Reference(), []string{}, &s)
+		if err != nil {
+			return fmt.Errorf("failed to get snapshot properties: %s", err)
+		}
+
+		for _, device := range s.Config.Hardware.Device {
+			switch disk := device.(type) {
+			case *types.VirtualDisk:
+				backing := disk.Backing.(types.BaseVirtualDeviceFileBackingInfo)
+				info := backing.GetVirtualDeviceFileBackingInfo()
+				snapbackingdisk = append(snapbackingdisk, info.FileName)
+				snapname = append(snapname, o.Snapshot.CurrentSnapshot.Value)
+				changeid, err := getChangeID(disk)
+				if err != nil {
+					return fmt.Errorf("failed to get change ID: %s", err)
+				}
+				snapid = append(snapid, changeid.Value)
+			}
+		}
+		for idx, _ := range vminfo.VMDisks {
+			if vminfo.VMDisks[idx].Name == disk.Name {
+				if blockCopySuccess {
+					vminfo.VMDisks[idx].ChangeID = snapid[idx]
+				}
+				vminfo.VMDisks[idx].SnapBackingDisk = snapbackingdisk[idx]
+				vminfo.VMDisks[idx].Snapname = snapname[idx]
+				log.Println(fmt.Sprintf("Updated disk info for %s", disk.Name))
+				log.Println(fmt.Sprintf("Snapshot backing disk: %s", snapbackingdisk[idx]))
+				log.Println(fmt.Sprintf("Snapshot name: %s", snapname[idx]))
+				log.Println(fmt.Sprintf("Change ID: %s", snapid[idx]))
+				break
+			}
+		}
+	}
+
+	return nil
 }
 
 func (vmops *VMOps) IsCBTEnabled() (bool, error) {
