d8c6be3f4eaa6d8e285765e87d6ff2c13bae0b1a
sarikap9 <sarika@platform9.com>
2025-06-27 14:57:44 +0530

    remove docs dir and optimize API doc generation

--- docs/_astro/content-modules.mjs
+++
@@ -1,5 +0,0 @@
-
-export default new Map([
-["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/introduction/getting_started.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fintroduction%2Fgetting_started.mdx&astroContentModuleFlag=true")]]);
-		
