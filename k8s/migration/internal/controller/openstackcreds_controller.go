45b9db083b3f8a67c9dbe35e8553d646e340f68f
sarikap9 <sarika@platform9.com>
2025-07-09 16:12:42 +0530

    test if it works with predicate filter

--- k8s/migration/internal/controller/openstackcreds_controller.go
+++ k8s/migration/internal/controller/openstackcreds_controller.go
@@ -34,6 +34,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
 )
 
 // OpenstackCredsReconciler reconciles a OpenstackCreds object
@@ -278,5 +279,6 @@ func (r *OpenstackCredsReconciler) reconcileDelete(ctx context.Context, scope *s
 func (r *OpenstackCredsReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&vjailbreakv1alpha1.OpenstackCreds{}).
+		WithEventFilter(predicate.GenerationChangedPredicate{}).
 		Complete(r)
 }
