0ab9a79e27d64dd77a8d1200132420aea4cce406
sarikap9 <sarika@platform9.com>
2025-07-09 09:28:09 +0530

    clean up the dependent PCDCluster

--- k8s/migration/internal/controller/openstackcreds_controller.go
+++ k8s/migration/internal/controller/openstackcreds_controller.go
@@ -95,7 +95,10 @@ func (r *OpenstackCredsReconciler) Reconcile(ctx context.Context, req ctrl.Reque
 
 	if !openstackcreds.DeletionTimestamp.IsZero() {
 		ctxlog.Info("Resource is being deleted, reconciling deletion", "openstackcreds", req.NamespacedName)
-		return r.reconcileDelete(ctx, scope)
+		if err := r.reconcileDelete(ctx, scope); err != nil {
+			return ctrl.Result{}, err
+		}
+		return ctrl.Result{}, nil
 	}
 	ctxlog.Info("Reconciling normal state", "openstackcreds", req.NamespacedName)
 	return r.reconcileNormal(ctx, scope)
@@ -224,45 +227,48 @@ func (r *OpenstackCredsReconciler) reconcileNormal(ctx context.Context,
 	return ctrl.Result{Requeue: true, RequeueAfter: constants.CredsRequeueAfter}, nil
 }
 
-func (r *OpenstackCredsReconciler) reconcileDelete(ctx context.Context,
-	scope *scope.OpenstackCredsScope) (ctrl.Result, error) { //nolint:unparam //future use
+func (r *OpenstackCredsReconciler) reconcileDelete(ctx context.Context, scope *scope.OpenstackCredsScope) error {
 	ctxlog := scope.Logger
 	ctxlog.Info("Reconciling deletion", "openstackcreds", scope.OpenstackCreds.Name, "namespace", scope.OpenstackCreds.Namespace)
-	// Delete the associated secret
-	client := r.Client
-	secretName := scope.OpenstackCreds.Spec.SecretRef.Name
+	openstackcreds := scope.OpenstackCreds
 
-	ctxlog.Info("Deleting PCD cluster", "openstackcreds", scope.OpenstackCreds.Name)
-	if err := utils.DeleteEntryForNoPCDCluster(ctx, client, scope.OpenstackCreds); err != nil {
-		ctxlog.Error(err, "Failed to delete PCD cluster", "openstackcreds", scope.OpenstackCreds.Name)
-		return ctrl.Result{}, errors.Wrap(err, "failed to delete PCD cluster")
+	if err := utils.DeleteEntryForNoPCDCluster(ctx, r.Client, openstackcreds); err != nil {
+		ctxlog.Error(err, "Failed to delete PCD cluster entry")
+		return fmt.Errorf("failed to delete PCD cluster entry: %w", err)
 	}
-
-	ctxlog.Info("Deleting associated secret", "secretName", secretName, "namespace", constants.NamespaceMigrationSystem)
-	err := client.Delete(ctx, &corev1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      secretName,
-			Namespace: constants.NamespaceMigrationSystem,
-		},
-	})
-	if err != nil && !apierrors.IsNotFound(err) {
-		ctxlog.Error(err, "Failed to delete associated secret", "secretName", secretName)
-		return ctrl.Result{}, errors.Wrap(err, "failed to delete associated secret")
+	if secretName := openstackcreds.Spec.SecretRef.Name; secretName != "" {
+		ctxlog.Info("Deleting associated secret", "secretName", secretName)
+		secret := &corev1.Secret{ObjectMeta: metav1.ObjectMeta{Name: secretName, Namespace: constants.NamespaceMigrationSystem}}
+		if err := r.Delete(ctx, secret); err != nil && !apierrors.IsNotFound(err) {
+			ctxlog.Error(err, "Failed to delete associated secret", "secretName", secretName)
+			return fmt.Errorf("failed to delete associated secret: %w", err)
+		}
+		ctxlog.Info("Successfully deleted associated secret or it was already gone", "secretName", secretName)
 	}
-	ctxlog.Info("Successfully deleted associated secret or it was already gone", "secretName", secretName)
-	ctxlog.Info("Removing finalizer", "finalizer", constants.OpenstackCredsFinalizer)
-	ctxlog.Info("All cleanup successful. Removing finalizer.")
-	if controllerutil.RemoveFinalizer(scope.OpenstackCreds, constants.OpenstackCredsFinalizer) {
-		if err := r.Update(ctx, scope.OpenstackCreds); err != nil {
-			if apierrors.IsNotFound(err) {
-				return ctrl.Result{}, nil
-			}
-			ctxlog.Error(err, "failed to update resource to remove finalizer")
-			return ctrl.Result{}, err
+	ctxlog.Info("Cleaning up associated PCDCluster resources")
+	pcdClusterList := &vjailbreakv1alpha1.PCDClusterList{}
+	labelSelector := client.MatchingLabels{constants.OpenstackCredsLabel: openstackcreds.Name}
+	if err := r.List(ctx, pcdClusterList, client.InNamespace(openstackcreds.Namespace), labelSelector); err != nil {
+		return fmt.Errorf("failed to list PCDClusters for cleanup: %w", err)
+	}
+	for i := range pcdClusterList.Items {
+		pcdCluster := pcdClusterList.Items[i]
+		ctxlog.Info("Deleting dependent PCDCluster", "name", pcdCluster.Name)
+		if err := r.Delete(ctx, &pcdCluster); err != nil && !apierrors.IsNotFound(err) {
+			return fmt.Errorf("failed to delete dependent PCDCluster %s: %w", pcdCluster.Name, err)
 		}
 	}
-
-	return ctrl.Result{}, nil
+	ctxlog.Info("All cleanup successful. Removing finalizer.")
+	if controllerutil.RemoveFinalizer(openstackcreds, constants.OpenstackCredsFinalizer) {
+		if err := r.Update(ctx, openstackcreds); err != nil {
+			if apierrors.IsNotFound(err) {
+				return nil
+			}
+			ctxlog.Error(err, "failed to update resource to remove finalizer")
+			return err
+		}
+	}
+	return nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
