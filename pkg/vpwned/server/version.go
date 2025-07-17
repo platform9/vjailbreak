e2afc43d492118521e7a26c18f4c4a17854e45ae
sarikap9 <sarika@platform9.com>
2025-07-17 09:29:18 +0530

    removed unused import

--- pkg/vpwned/server/version.go
+++ pkg/vpwned/server/version.go
@@ -24,7 +24,7 @@ import (
 	"sigs.k8s.io/yaml"
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/client-go/kubernetes/typed/core/v1"
+	corev1 "k8s.io/api/core/v1"
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
 )
  
