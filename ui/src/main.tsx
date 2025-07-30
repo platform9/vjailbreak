a3a8301227fe936160ae0bb6b9721360cece1410
Pratik Patil <63217589+patil-pratik-87@users.noreply.github.com>
2025-07-30 10:03:31 +0530

    added amplitude changes (#765)

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -4,41 +4,18 @@ import "@fontsource/roboto/500.css"
 import "@fontsource/roboto/700.css"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
-import React, { StrictMode } from "react"
+import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
-import Bugsnag from '@bugsnag/js'
-import BugsnagPluginReact from '@bugsnag/plugin-react'
-import BugsnagPerformance from '@bugsnag/browser-performance'
 import App from "./App.tsx"
 import { ThemeProvider } from "./theme/ThemeContext.tsx"
-import { getBugsnagConfig, getBugsnagPerformanceConfig } from "./config/bugsnag"
-import { errorReportingService } from "./services/errorReporting"
+import { AnalyticsProvider } from "./components/AnalyticsProvider.tsx"
 
 const queryClient = new QueryClient()
 
-const bugsnagConfig = getBugsnagConfig()
-const bugsnagPerformanceConfig = getBugsnagPerformanceConfig()
-
-if (bugsnagConfig.apiKey) {
-  Bugsnag.start({
-    ...bugsnagConfig,
-    plugins: [new BugsnagPluginReact()],
-  })
-
-  BugsnagPerformance.start(bugsnagPerformanceConfig)
-
-  errorReportingService.initialize(Bugsnag)
-
-  errorReportingService.addMetadata('app', 'name', 'vjailbreak')
-  errorReportingService.addMetadata('app', 'component', 'ui')
-}
-
-const ErrorBoundary = Bugsnag.getPlugin('react')?.createErrorBoundary(React) || React.Fragment
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ErrorBoundary>
+    <AnalyticsProvider>
       <BrowserRouter>
         <ThemeProvider>
           <QueryClientProvider client={queryClient}>
@@ -47,6 +24,7 @@ createRoot(document.getElementById("root")!).render(
           </QueryClientProvider>
         </ThemeProvider>
       </BrowserRouter>
-    </ErrorBoundary>
+    </AnalyticsProvider>
   </StrictMode>
 )
+
