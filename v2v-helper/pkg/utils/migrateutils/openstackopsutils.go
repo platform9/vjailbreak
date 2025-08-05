8e69582189be57e8937d5b618aaa24ae280c1ad2
Omkar Deshpande <omideshpande77@gmail.com>
2025-08-05 14:32:38 +0000

    address feedback

--- v2v-helper/pkg/utils/migrateutils/openstackopsutils.go
+++ v2v-helper/pkg/utils/migrateutils/openstackopsutils.go
@@ -410,18 +410,21 @@ func (osclient *OpenStackClients) CreatePort(network *networks.Network, mac, ip,
 
 	port, err := ports.Create(osclient.NetworkingClient, createOpts).Extract()
 	if err != nil {
-		// return nil, err
+		// Static IP assignment failed, fall back to DHCP
 		utils.PrintLog(fmt.Sprintf("Could Not Use IP: %s, using DHCP to create Port", ip))
-		_, err = ports.Create(osclient.NetworkingClient, ports.CreateOpts{
+		dhcpPort, dhcpErr := ports.Create(osclient.NetworkingClient, ports.CreateOpts{
 			Name:           "port-" + vmname,
 			NetworkID:      network.ID,
 			MACAddress:     mac,
 			SecurityGroups: &securityGroups,
 		}).Extract()
-		if err != nil {
-			return nil, errors.Wrap(err, "failed to create port with static IP")
+		
+		if dhcpErr != nil {
+			return nil, errors.Wrap(dhcpErr, "failed to create port with DHCP after static IP failed")
 		}
-		return nil, fmt.Errorf("failed to create port with static IP %s. Error: %w", ip, err)
+		
+		utils.PrintLog(fmt.Sprintf("Port created with DHCP instead of static IP %s. Port ID: %s", ip, dhcpPort.ID))
+		return dhcpPort, nil
 	}
 
 	utils.PrintLog(fmt.Sprintf("Port created with ID: %s", port.ID))
