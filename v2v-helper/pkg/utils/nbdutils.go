7a759bda4e8edcc98d920d489abc24c95393d6a6
Omkar Deshpande <omideshpande77@gmail.com>
2025-08-20 09:01:22 +0000

    close open files delete the old ones

--- v2v-helper/pkg/utils/nbdutils.go
+++ v2v-helper/pkg/utils/nbdutils.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"sync"
 	"time"
 
 	"github.com/platform9/vjailbreak/v2v-helper/pkg/constants"
@@ -16,6 +17,38 @@ func ParseFraction(text string) (int, int, error) {
 		return 0, 0, err
 	}
 	return numerator, denominator, nil
+}
+
+// openLogFiles tracks active log files that need to be closed properly
+var openLogFiles = make(map[*exec.Cmd]*os.File)
+var logFileMutex sync.Mutex
+
+// CloseLogFile closes the log file associated with a command
+func CloseLogFile(cmd *exec.Cmd) {
+	logFileMutex.Lock()
+	defer logFileMutex.Unlock()
+	
+	if logFile, exists := openLogFiles[cmd]; exists {
+		PrintLog(fmt.Sprintf("Closing log file: %s", logFile.Name()))
+		logFile.Sync()
+		logFile.Close()
+		delete(openLogFiles, cmd)
+	}
+}
+
+// RunCommandWithLogFile runs a command with output redirected to a log file
+// and ensures the log file is properly closed after execution
+func RunCommandWithLogFile(cmd *exec.Cmd) error {
+	// First ensure output is directed to a log file
+	AddDebugOutputToFile(cmd)
+	
+	// Run the command
+	err := cmd.Run()
+	
+	// Close the log file
+	CloseLogFile(cmd)
+	
+	return err
 }
 
 func AddDebugOutputToFile(cmd *exec.Cmd) {
@@ -50,15 +83,27 @@ func AddDebugOutputToFile(cmd *exec.Cmd) {
 	// Create an attempt-specific log file
 	logFilePath := fmt.Sprintf("%s/attempt-%d-%s.log", attemptDir, attemptNum, timestamp)
 	
+	// First close any existing log file for this command
+	CloseLogFile(cmd)
+	
 	// Create new file or append if it already exists (same attempt)
 	logFile, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
 		return
 	}
 	
+	// Keep track of the open file so we can close it later
+	logFileMutex.Lock()
+	openLogFiles[cmd] = logFile
+	logFileMutex.Unlock()
+	
 	PrintLog(fmt.Sprintf("Debug mode enabled. Command output will be logged to %s", logFilePath))
 
 	// Set stdout/stderr to only write to the log file, not to kubectl logs
 	cmd.Stdout = logFile
 	cmd.Stderr = logFile
+	
+	// NOTE: The log file will NOT be automatically closed.
+	// Either call CloseLogFile(cmd) manually after the command completes,
+	// or use RunCommandWithLogFile() which handles the cleanup automatically.
 }
