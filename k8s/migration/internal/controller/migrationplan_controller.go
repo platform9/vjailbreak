e48abaf1a66e654ffa639d103084f690f4522ff4
Suhas <suhas@platform9.com>
2025-08-23 01:21:56 +0530

    add multip

--- k8s/migration/internal/controller/migrationplan_controller.go
+++ k8s/migration/internal/controller/migrationplan_controller.go
@@ -938,12 +938,19 @@ func (r *MigrationPlanReconciler) reconcileNetwork(ctx context.Context,
 
 	openstacknws := []string{}
 	for _, vmnw := range vmnws {
+		found := false
 		for _, nwm := range networkmap.Spec.Networks {
 			if vmnw == nwm.Source {
 				openstacknws = append(openstacknws, nwm.Target)
+				found = true
+				break // avoid duplicate matches
 			}
 		}
+		if !found {
+			return nil, errors.Errorf("VMware network %q not found in NetworkMapping", vmnw)
+		}
 	}
+
 	if len(openstacknws) != len(vmnws) {
 		return nil, errors.Errorf("VMware Network(s) not found in NetworkMapping vm(%d) openstack(%d)", len(vmnws), len(openstacknws))
 	}
