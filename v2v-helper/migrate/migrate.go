4b90d692ca65ecc8ed2f7c80c016405357df621c
Omkar Deshpande <omideshpande77@gmail.com>
2025-09-03 10:09:39 +0000

    remove 24 hr sleep

--- v2v-helper/migrate/migrate.go
+++ v2v-helper/migrate/migrate.go
@@ -413,8 +413,6 @@ func (migobj *Migrate) LiveReplicateDisks(ctx context.Context, vminfo vm.VMInfo)
 
 	}
 
-	time.Sleep(24 * time.Hour)
-
 	err = migobj.DetachAllVolumes(vminfo)
 	if err != nil {
 		return vminfo, errors.Wrap(err, "Failed to detach all volumes from VM")
