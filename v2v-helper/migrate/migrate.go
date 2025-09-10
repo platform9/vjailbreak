609c0139563f474799c5c332136f75c7b3221533
Rishabh Mishra <rishabhmishra625@gmail.com>
2025-09-10 11:22:06 +0530

    bugfix: Add logic to assign IP from network interfaces if not already set

--- v2v-helper/migrate/migrate.go
+++ v2v-helper/migrate/migrate.go
@@ -838,7 +838,14 @@ func (migobj *Migrate) CreateTargetInstance(vminfo vm.VMInfo) error {
 			} else {
 				ip = vminfo.IPs[idx]
 			}
-
+			if ip == "" {
+				for _, nic := range vminfo.NetworkInterfaces {
+					if nic.MAC == vminfo.Mac[idx] {
+						ip = nic.IPAddress
+						break
+					}
+				}
+			}
 			if migobj.AssignedIP != "" {
 				ip = migobj.AssignedIP
 			}
