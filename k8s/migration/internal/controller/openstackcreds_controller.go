8caf267a3321e6a14bf6eb2140fe5db541972402
Suhas <suhas@platform9.com>
2025-09-12 12:29:48 +0530

    remove parallel mig

--- k8s/migration/internal/controller/openstackcreds_controller.go
+++ k8s/migration/internal/controller/openstackcreds_controller.go
@@ -31,8 +31,6 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
