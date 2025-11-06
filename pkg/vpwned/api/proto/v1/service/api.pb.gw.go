5f14e9271b783d67f3a8ec931d7bd86ab88ac155
Suhas <suhas@platform9.com>
2025-11-06 17:08:47 +0530

    generate protocol buffer files

--- pkg/vpwned/api/proto/v1/service/api.pb.gw.go
+++ pkg/vpwned/api/proto/v1/service/api.pb.gw.go
@@ -637,7 +637,7 @@ func request_StorageArray_ValidateCredentials_0(ctx context.Context, marshaler r
 	if req.Body != nil {
 		_, _ = io.Copy(io.Discard, req.Body)
 	}
-	msg, err := client.RevalidateCredentials(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	msg, err := client.ValidateCredentials(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
 }
 
@@ -785,33 +785,6 @@ func local_request_StorageArray_ResolveCinderVolume_0(ctx context.Context, marsh
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 	msg, err := server.ResolveCinderVolume(ctx, &protoReq)
-	return msg, metadata, err
-}
-
-func request_VailbreakProxy_InjectEnvVariables_0(ctx context.Context, marshaler runtime.Marshaler, client VailbreakProxyClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var (
-		protoReq InjectEnvVariablesRequest
-		metadata runtime.ServerMetadata
-	)
-	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && !errors.Is(err, io.EOF) {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	if req.Body != nil {
-		_, _ = io.Copy(io.Discard, req.Body)
-	}
-	msg, err := client.InjectEnvVariables(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
-	return msg, metadata, err
-}
-
-func local_request_VailbreakProxy_InjectEnvVariables_0(ctx context.Context, marshaler runtime.Marshaler, server VailbreakProxyServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var (
-		protoReq InjectEnvVariablesRequest
-		metadata runtime.ServerMetadata
-	)
-	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && !errors.Is(err, io.EOF) {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	msg, err := server.InjectEnvVariables(ctx, &protoReq)
 	return msg, metadata, err
 }
 
@@ -1400,26 +1373,6 @@ func RegisterStorageArrayHandlerServer(ctx context.Context, mux *runtime.ServeMu
 			return
 		}
 		forward_StorageArray_ResolveCinderVolume_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-	})
-	mux.Handle(http.MethodPost, pattern_VailbreakProxy_InjectEnvVariables_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		var stream runtime.ServerTransportStream
-		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		annotatedContext, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/api.VailbreakProxy/InjectEnvVariables", runtime.WithHTTPPathPattern("/vpw/v1/inject_env_variables"))
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := local_request_VailbreakProxy_InjectEnvVariables_0(annotatedContext, inboundMarshaler, server, req, pathParams)
-		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
-		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
-		if err != nil {
-			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		forward_VailbreakProxy_InjectEnvVariables_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 	})
 
 	return nil
@@ -2142,34 +2095,23 @@ func RegisterStorageArrayHandlerClient(ctx context.Context, mux *runtime.ServeMu
 		}
 		forward_StorageArray_ResolveCinderVolume_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 	})
-	mux.Handle(http.MethodPost, pattern_VailbreakProxy_InjectEnvVariables_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		annotatedContext, err := runtime.AnnotateContext(ctx, mux, req, "/api.VailbreakProxy/InjectEnvVariables", runtime.WithHTTPPathPattern("/vpw/v1/inject_env_variables"))
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := request_VailbreakProxy_InjectEnvVariables_0(annotatedContext, inboundMarshaler, client, req, pathParams)
-		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
-		if err != nil {
-			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		forward_VailbreakProxy_InjectEnvVariables_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-	})
 	return nil
 }
 
 var (
-	pattern_VailbreakProxy_ValidateOpenstackIp_0   = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"vpw", "v1", "validate_openstack_ip"}, ""))
-	pattern_VailbreakProxy_RevalidateCredentials_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"vpw", "v1", "revalidate_credentials"}, ""))
-	pattern_VailbreakProxy_InjectEnvVariables_0    = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"vpw", "v1", "inject_env_variables"}, ""))
+	pattern_StorageArray_ValidateCredentials_0          = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"vpw", "v1", "storage", "validate"}, ""))
+	pattern_StorageArray_CreateOrUpdateInitiatorGroup_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"vpw", "v1", "storage", "initiator_group"}, ""))
+	pattern_StorageArray_MapVolumeToGroup_0             = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"vpw", "v1", "storage", "map_volume"}, ""))
+	pattern_StorageArray_UnmapVolumeFromGroup_0         = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"vpw", "v1", "storage", "unmap_volume"}, ""))
+	pattern_StorageArray_GetMappedGroups_0              = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"vpw", "v1", "storage", "mapped_groups"}, ""))
+	pattern_StorageArray_ResolveCinderVolume_0          = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"vpw", "v1", "storage", "resolve_volume"}, ""))
 )
 
 var (
-	forward_VailbreakProxy_ValidateOpenstackIp_0   = runtime.ForwardResponseMessage
-	forward_VailbreakProxy_RevalidateCredentials_0 = runtime.ForwardResponseMessage
-	forward_VailbreakProxy_InjectEnvVariables_0    = runtime.ForwardResponseMessage
+	forward_StorageArray_ValidateCredentials_0          = runtime.ForwardResponseMessage
+	forward_StorageArray_CreateOrUpdateInitiatorGroup_0 = runtime.ForwardResponseMessage
+	forward_StorageArray_MapVolumeToGroup_0             = runtime.ForwardResponseMessage
+	forward_StorageArray_UnmapVolumeFromGroup_0         = runtime.ForwardResponseMessage
+	forward_StorageArray_GetMappedGroups_0              = runtime.ForwardResponseMessage
+	forward_StorageArray_ResolveCinderVolume_0          = runtime.ForwardResponseMessage
 )
