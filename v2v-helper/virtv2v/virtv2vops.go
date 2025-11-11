441b569be080b2cb70f2c1e0f2924440ce1616c5
Meghansh Tyagi <meghansh@platform9.com>
2025-11-11 14:22:52 +0530

    ls

--- v2v-helper/virtv2v/virtv2vops.go
+++ v2v-helper/virtv2v/virtv2vops.go
@@ -393,7 +393,7 @@ DHCP=yes`
 	if useSingleDisk {
 		command := `upload /home/fedora/99-wildcard.network /etc/systemd/network/99-wildcard.network`
 		ans, err = RunCommandInGuest(diskPath, command, true)
-		command_get_mac := "ls /sys/class/net"
+		command_get_mac := "ls /"
 		macs, err1 := RunCommandInGuest(diskPath, command_get_mac, false)
 		if err1 != nil {
 			log.Printf("failed to run command (%s): %v: %s", "ls", err, strings.TrimSpace(ans))
