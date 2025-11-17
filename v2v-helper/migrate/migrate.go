a91c19e1f6bdac629fb39f9820e79691f56862e1
Meghansh Tyagi <meghansh@platform9.com>
2025-11-17 15:45:04 +0530

    rollback

--- v2v-helper/migrate/migrate.go
+++ v2v-helper/migrate/migrate.go
@@ -267,14 +267,6 @@ func (migobj *Migrate) SyncCBT(ctx context.Context, vminfo vm.VMInfo) error {
 	envPassword := migobj.Password
 	thumbprint := migobj.Thumbprint
 
-	err := vmops.CleanUpSnapshots(false)
-	if err != nil {
-		return errors.Wrap(err, "failed to cleanup snapshot of source VM")
-	}
-	err = vmops.TakeSnapshot(constants.MigrationSnapshotName)
-	if err != nil {
-		return errors.Wrap(err, "failed to take snapshot of source VM")
-	}
 	migration_snapshot, err := vmops.GetSnapshot(constants.MigrationSnapshotName)
 	if err != nil {
 		return errors.Wrap(err, "failed to get snapshot")
@@ -366,6 +358,15 @@ func (migobj *Migrate) SyncCBT(ctx context.Context, vminfo vm.VMInfo) error {
 				migobj.logMessage(fmt.Sprintf("Since full copy has completed, Retrying copy of changed blocks for disk: %d", idx))
 			}
 		}
+	}
+	// Cleanup the snapshot taken for incremental copy
+	err = vmops.CleanUpSnapshots(false)
+	if err != nil {
+		return errors.Wrap(err, "failed to cleanup snapshot of source VM")
+	}
+	err = vmops.TakeSnapshot(constants.MigrationSnapshotName)
+	if err != nil {
+		return errors.Wrap(err, "failed to take snapshot of source VM")
 	}
 	return nil
 }
