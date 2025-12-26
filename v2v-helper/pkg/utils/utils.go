fbe5b4842141458b94bca7ffcf70aeba889c08be
meghansh-pf9 <meghansh@platform9.com>
2025-12-26 18:40:57 +0530

    Updated Backend

--- v2v-helper/pkg/utils/utils.go
+++ v2v-helper/pkg/utils/utils.go
@@ -172,3 +172,10 @@ func DoRetryWithExponentialBackoff(ctx context.Context, task func() error, maxRe
 	}
 	return err
 }
+func GetNetworkPersistance(ctx context.Context, client client.Client) bool {
+	migrationParams, err := GetMigrationParams(ctx, client)
+	if err != nil {
+		return false
+	}
+	return migrationParams.NetworkPersistance
+}
