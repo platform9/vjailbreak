27ad4ff83853eb3025dbf66a432817a29ff1fdf8
meghansh-pf9 <meghansh@platform9.com>
2026-01-13 10:44:46 +0530

    Getting Registry

--- scripts/get-windows-interfaces.sh
+++ scripts/get-windows-interfaces.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+if [ -f /home/fedora/SYSTEM ]; then
+    if [ ! -d /home/fedora/registry ]; then
+        mkdir /home/fedora/registry
+    fi
+    reged -x /home/fedora/SYSTEM "HKEY_LOCAL_MACHINE\SYSTEM" "\ControlSet001\Control\Network" /home/fedora/registry/network.reg
+    reged -x /home/fedora/SYSTEM "HKEY_LOCAL_MACHINE\SYSTEM" "\ControlSet001\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}" /home/fedora/registry/class.reg
+    reged -x /home/fedora/SYSTEM "HKEY_LOCAL_MACHINE\SYSTEM" "\ControlSet001\Services" /home/fedora/registry/service.reg
+fi
