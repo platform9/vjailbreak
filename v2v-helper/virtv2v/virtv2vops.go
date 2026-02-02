82466ac4d25edc526d81cd23c4150b242ceec4c8
Omkar Deshpande <omideshpande7@gmail.com>
2026-02-02 09:46:11 +0000

    add a firstboot with newer virt-v2v version

--- v2v-helper/virtv2v/virtv2vops.go
+++ v2v-helper/virtv2v/virtv2vops.go
@@ -229,6 +229,9 @@ func ConvertDisk(ctx context.Context, xmlFile, path, ostype, virtiowindriver str
 	for _, script := range firstbootscripts {
 		args = append(args, "--firstboot", fmt.Sprintf("/home/fedora/%s.sh", script))
 	}
+	if strings.Contains(strings.ToLower(osRelease), "server 2012") || strings.Contains(strings.ToLower(osRelease), "server2012") {
+		args = append(args, "--firstboot", "/home/fedora/scripts/install-virtio-win12")
+	}
 	if useSingleDisk {
 		args = append(args, "-i", "disk", diskPath)
 	} else {
