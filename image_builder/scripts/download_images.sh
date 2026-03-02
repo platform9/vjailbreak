755f4eed26dcd0c78c84fd3991c9741078d95b35
Suhas <suhas@platform9.com>
2026-03-02 23:19:27 +0530

    Final image download push

--- image_builder/scripts/download_images.sh
+++ image_builder/scripts/download_images.sh
@@ -14,11 +14,11 @@ kube_state_metrics="registry.k8s.io/kube-state-metrics/kube-state-metrics:v2.18.
 prometheus_adapter="registry.k8s.io/prometheus-adapter/prometheus-adapter:v0.12.0"
 prometheus="quay.io/prometheus/prometheus:v3.9.1"
 alertmanager="quay.io/prometheus/alertmanager:v0.31.1"
-blackbox_exporter="quay.io/prometheus/blackbox-exporter:v0.25.0"
+blackbox_exporter="quay.io/prometheus/blackbox-exporter:v0.28.0"
 node_exporter="quay.io/prometheus/node-exporter:v1.10.2"
 pushgateway="quay.io/prometheus/pushgateway:v1.5.0"
 kube_rbac_proxy="quay.io/brancz/kube-rbac-proxy:v0.20.2"
-prometheus_config_reloader="quay.io/prometheus-operator/prometheus-config-reloader:v0.76.0"
+prometheus_config_reloader="quay.io/prometheus-operator/prometheus-config-reloader:v0.89.0"
 prometheus_operator="quay.io/prometheus-operator/prometheus-operator:v0.89.0"
 configmap_reload="ghcr.io/jimmidyson/configmap-reload:v0.15.0"
 grafana="docker.io/grafana/grafana:12.3.3"
@@ -38,8 +38,8 @@ cert_manager_cainjector="quay.io/jetstack/cert-manager-cainjector:${CERT_MANAGER
 
 INGRESS_NGINX_VERSION="v1.14.3"
 KUBE_WEBHOOK_CERTGEN_VERSION="v1.6.7"
-ingress_nginx_controller="registry.k8s.io/ingress-nginx/controller@${INGRESS_NGINX_VERSION}"
-kube_webhook_certgen="registry.k8s.io/ingress-nginx/kube-webhook-certgen@${KUBE_WEBHOOK_CERTGEN_VERSION}"
+ingress_nginx_controller="registry.k8s.io/ingress-nginx/controller:${INGRESS_NGINX_VERSION}@sha256:82917be97c0939f6ada1717bb39aa7e66c229d6cfb10dcfc8f1bd42f9efe0f81"
+kube_webhook_certgen="registry.k8s.io/ingress-nginx/kube-webhook-certgen:${KUBE_WEBHOOK_CERTGEN_VERSION}@sha256:7c74a715af2c94cb734785b4d3ea1357b4f02b88e1e123c622a9cb68b62f669c"
 
 # Download cert-manager manifests
 CERT_MANAGER_URL="https://github.com/cert-manager/cert-manager/releases/download/${CERT_MANAGER_VERSION}/cert-manager.yaml"
@@ -83,7 +83,7 @@ for img in "${images[@]}"; do
   fname=$(echo "$tag" | tr '/:@' '_')
 
   echo "[*] Exporting to $fname.tar"
-  sudo ctr i export "image_builder/images/$fname.tar" "$img"
+  sudo ctr  i export "image_builder/images/$fname.tar" "$img"
 done
 
 
