aa5b50602ed516f76029ef491013dafe21bf06c3
OmkarDeshpande7 <omideshpande77@gmail.com>
2026-03-05 23:05:16 +0530

    fix controller

--- k8s/migration/pkg/utils/credutils.go
+++ k8s/migration/pkg/utils/credutils.go
@@ -404,6 +404,7 @@ func ValidateAndGetProviderClient(ctx context.Context, k3sclient client.Client,
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to get openstack credentials from secret")
 	}
+	openstackCredential.VJBInstanceID = openstackcreds.Spec.VJBInstanceID
 
 	providerClient, err := openstack.NewClient(openstackCredential.AuthURL)
 	if err != nil {
@@ -448,6 +449,10 @@ func ValidateAndGetProviderClient(ctx context.Context, k3sclient client.Client,
 		default:
 			return nil, fmt.Errorf("authentication failed: %w. Please verify your OpenStack credentials", err)
 		}
+	}
+
+	if openstackCredential.VJBInstanceID != "" {
+		return providerClient, nil
 	}
 
 	_, err = VerifyCredentialsMatchCurrentEnvironment(providerClient, openstackCredential.RegionName)
