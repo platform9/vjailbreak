201d3aa1523a26456227d5e54f8429f4a67909b1
meghansh-pf9 <meghansh@platform9.com>
2026-03-26 16:31:35 +0530

    removing fstrim for testing

--- v2v-helper/virtv2v/virtv2vops.go
+++ v2v-helper/virtv2v/virtv2vops.go
@@ -410,7 +410,7 @@ func ConvertDisk(ctx context.Context, xmlFile, path, ostype, virtiowindriver str
 	os.Setenv("LIBGUESTFS_BACKEND", "direct")
 
 	// Step 3: Prepare virt-v2v args
-	args := []string{"-v", "--no-fstrim"}
+	args := []string{"-v"}
 
 	if strings.ToLower(ostype) == constants.OSFamilyLinux {
 		userWrapperPath, err := prepareLinuxUserFirstBootWrapper(ostype)
