c5e0d792d5255d062596c51d9aff291a2fe32957
Sarika Gautam <sarika@platform9.com>
2026-04-28 10:06:32 +0530

    Pass vmkey instead of vmname to delete object (#1865)

--- ui/src/features/migration/pages/MigrationsPage.tsx
+++ ui/src/features/migration/pages/MigrationsPage.tsx
@@ -77,7 +77,8 @@ export default function MigrationsPage() {
               migrationsToDelete: new Set<string>()
             }
           }
-          acc[planId].vmsToRemove.add(migration.spec.vmName)
+          const vmKey = migration.metadata?.labels?.['vjailbreak.k8s.pf9.io/vm-key'] || migration.spec.vmName
+          acc[planId].vmsToRemove.add(vmKey)
           acc[planId].migrationsToDelete.add(migration.metadata.name)
           return acc
         },
